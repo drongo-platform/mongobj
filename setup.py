6b88b79e5364f79a2651dc52f0f20248080ec6c3
Sattvik Chakravarthy <sattvik@gmail.com>
2018-11-21 16:44:24 +0530

    Initial checkin.

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name='mongobj',
+    version='1.0.0',
+    description='Mongo object mapping.',
+    author='Sattvik Chakravarthy',
+    author_email='sattvik@gmail.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+)
